2d0155de0e3a5da0c40f96448950963b9048d332
AkashiZai <motherfcuker11@gmail.com>
2024-09-09 19:14:58 +0700

    Login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -22,20 +22,41 @@ function Login() {
                     </li> 
                     <li>
                         <a href="/">| คำนวณค่าขนส่ง</a>
-                    </li>
+                    </li> 
                 </ul>
             </nav>
         </header>
-
+    <div className="containerLogin">
         <div className="contentLogin">
           <h1>Login</h1>
           <form>
-              <input placeholder="username"/>
               <input placeholder="email address"/>
               <input placeholder="password"/>
-              <input placeholder="confirm password"/>
           </form>
         </div>
+    </div>
+    <footer>
+        <div className="logo">
+            <img src={Logo} alt="logo"/>
+        </div>
+        <div className="navfooter">
+        <ul>
+                    <li>
+                        <a href="/">| Home</a>
+                    </li> 
+                    <li>
+                        <a href="/">| About</a>
+                    </li> 
+                    <li>
+                        <a href="/">| ติดตามพัสดุ</a>
+                    </li> 
+                    <li>
+                        <a href="/">| คำนวณค่าขนส่ง</a>
+                    </li>
+                </ul>
+        </div>
+        <h1>Contact</h1>
+    </footer>
   </div>
   );
 }
