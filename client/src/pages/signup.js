25af8feed6b0500a0ddce3bdf10ab039fa5db584
AkashiZai <motherfcuker11@gmail.com>
2024-09-10 19:11:29 +0700

    DWKLKDKMQDQ
    dqwqwdqwd

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -32,7 +32,7 @@ function Signup() {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b111d1] md:text-4xl ">
+              <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b23cca] md:text-4xl ">
                 SIGN-UP
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
