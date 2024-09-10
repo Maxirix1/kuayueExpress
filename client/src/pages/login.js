fa64972ec92b9b45f85b566f67e85c1c93a23a3d
AkashiZai <motherfcuker11@gmail.com>
2024-09-10 19:08:03 +0700

    NEW

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -36,7 +36,7 @@ function Login() {
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b111d1] md:text-4xl">
+                            <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b410d4] md:text-4xl">
                                 LOGIN
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
