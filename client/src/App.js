76723849a49859a40d2b732b575cab835133c767
Maxirix <166589012+Maxirix1@users.noreply.github.com>
2024-09-10 19:48:31 +0700

    add tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './style/main.css';
 import Home from './pages/home'
 import Login from './pages/login'
+import Signup from './pages/signup'
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
   );
 }
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -46,8 +46,8 @@ function Navbar() {
         >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
             <li>
-              <Link
-                href="#"
+              <Link 
+                to="/"
                 className="block py-2 px-3 text-white bg-[#7d00d1] rounded md:bg-transparent md:text-[#7d00d1] md:p-0"
               >
                 Home
