d441de106841e90e21bc66dc771d09f216ef1f52
Maxirix <166589012+Maxirix1@users.noreply.github.com>
2024-09-10 12:25:11 +0700

    remove dark: in tailwind

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -34,26 +34,26 @@ function Login() {
 
             <section className="bg-gray-50">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b111d1] md:text-4xl dark:text-white">
+                            <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b111d1] md:text-4xl">
                                 LOGIN
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
-                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter email address" required />
+                                    <label className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Enter email address" required />
                                 </div>
                                 <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
                                 </div>
                                 {/* <div className="flex items-center justify-between">
                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
               </div> */}
-                                <button type="submit" className="w-full text-white bg-[#b111d1] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
-                                <p className="text-center text-sm font-light text-gray-500 dark:text-gray-400">
-                                    ยังไม่มีบัญชีใช่ไหม? <Link to="/signup" className="font-medium text-[#b111d1] hover:underline dark:text-primary-500">สมัครสมาชิก</Link> เลย!
+                                <button type="submit" className="w-full text-white bg-[#b111d1] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login</button>
+                                <p className="text-center text-sm font-light text-gray-500 ">
+                                    ยังไม่มีบัญชีใช่ไหม? <Link to="/signup" className="font-medium text-[#b111d1] hover:underline">สมัครสมาชิก</Link> เลย!
                                 </p>
                             </form>
                         </div>
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -28,36 +28,36 @@ function Signup() {
         </nav>
       </header>
 
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className="bg-gray-50 ">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b111d1] md:text-4xl dark:text-white">
+              <h1 className="text-center text-5xl font-bold leading-tight tracking-tight text-[#b111d1] md:text-4xl ">
                 SIGN-UP
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
-                  <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter username" required />
+                  <label className="block mb-2 text-sm font-medium text-gray-900 ">Your username</label>
+                  <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Enter username" required />
                 </div>
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter email address" required />
+                  <label className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Enter email address" required />
                 </div>
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  <label className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required />
                 </div>
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  <label className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
+                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required />
                 </div>
                 {/* <div className="flex items-center justify-between">
       <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
     </div> */}
-                <button type="submit" className="w-full text-white bg-[#b111d1] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                <p className="text-center text-sm font-light text-gray-500 dark:text-gray-400">
-                  มีบัญชีแล้วใช่ไหม? <Link to="/login" className="font-medium text-[#b111d1] hover:underline dark:text-primary-500">เข้าสู่ระบบ</Link> เลย!
+                <button type="submit" className="w-full text-white bg-[#b111d1] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign in</button>
+                <p className="text-center text-sm font-light text-gray-500 ">
+                  มีบัญชีแล้วใช่ไหม? <Link to="/login" className="font-medium text-[#b111d1] hover:underline ">เข้าสู่ระบบ</Link> เลย!
                 </p>
               </form>
             </div>
